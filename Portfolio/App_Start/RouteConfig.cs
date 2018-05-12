435350f0245853cf0e9840fd81fb50d052578ff3
Gary Taylor <gptaylor@yorkcountypa.gov>
2018-05-12 08:08:00 -0400

    VS is Being Dumb

--- Portfolio/App_Start/RouteConfig.cs
+++ Portfolio/App_Start/RouteConfig.cs
@@ -13,6 +13,25 @@ namespace Portfolio
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           routes.MapRoute(
+                name: "CSharp",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "CSharp", action = "Examples", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Web Design",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "WebDesign", action = "Examples", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Web Development",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "WebDevelopment", action = "Examples", id = UrlParameter.Optional }
+            );
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Portfolio/Models/ContactRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class ContactRequest
+    {
+        [Display(Name = "Name: ", Description = "Your name.")]
+        public string Name { get; set; }
+        [Display(Name = "Phone Number: ", Description = "Phone number to be reached at.")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Email: ", Description = "Email number to be reached at.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Comment/Question ", Description = "What would you like to say?")]
+        public string CommentQuestion { get; set; }
+    }
+}
